a2bd961db9c0814770ba8748193a5cac6a9cf46f
favour asuk <favourasuk@gmail.com>
2024-05-15 17:03:43 +0100

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,19 @@
 // components/Header.jsx
-import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../Styles/Header.css';
 import logo from '../assets/Silvvy_logo_pink.png';
 
 const Header = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
+  
 
   return (
     <header className="header">
       <div className="header-logo">
         <img src={logo} alt="Logo" />
       </div>
-      <nav className={`header-navigation ${menuOpen ? 'open' : ''}`}>
-        <div className="menu-toggle" onClick={toggleMenu}>
-          <div className="bar"></div>
-          <div className="bar"></div>
-          <div className="bar"></div>
-        </div>
+      <nav className={`header-navigation `}>
+        
+     
         <ul>
           <li>
             <Link to="/">Home</Link>
